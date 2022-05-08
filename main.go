f5f5777b9f3fa86b070f53095ab9cdd8b86948f9
Orochyy <yura333619@gmail.com>
2022-05-08 17:45:03 +0300

    BTC wallet

--- main.go
+++ main.go
@@ -7,30 +7,33 @@ import (
 	"golang.org/x/net/context"
 )
 
+type wallet struct {
+	BTC float64 `json:"btc"`
+}
+
 var (
 	apiKey    = "wdA1ZXkD2VgF7p3v1vgI8CkgsXXoT1IqFCTeo8XAWDbAnitoFry4Z53iCqCJsBhU"
 	secretKey = "6RysiPFMfRTYOA3mN5JcfUwQLKfM7YD4QglmIsoH1W5OwQWQgEtRd54JssQQ1fAA"
 )
 var client = binance.NewClient(apiKey, secretKey)
-var futuresClient = binance.NewFuturesClient(apiKey, secretKey)   // USDT-M Futures
-var deliveryClient = binance.NewDeliveryClient(apiKey, secretKey) // Coin-M Futures
 
-func startUserStream() {
-	res, err := client.NewStartUserStreamService().Do(context.Background())
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(res)
-}
+//func startUserStream() {
+//	res, err := client.NewStartUserStreamService().Do(context.Background())
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//	fmt.Println(res)
+//}
 
-func userWallet(*gin.Context) {
+func userWalletBTC(*gin.Context) {
+	var result []binance.Balance
 	res, err := client.NewGetAccountService().Do(context.Background())
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
-	fmt.Println(res)
+	result = res.Balances
+	fmt.Println(result[0])
 }
 
 //func userData() {
@@ -48,14 +51,13 @@ func userWallet(*gin.Context) {
 //	<-doneC
 //}
 func main() {
-
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.GET("/wallet", userWallet)
+	r.GET("/wallet", userWalletBTC)
 	r.Run(":8080")
 
 }
