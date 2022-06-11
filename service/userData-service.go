c32f1a6b97f6ae46ba7d7aaa4cc320d523214a29
Orochyy <yura333619@gmail.com>
2022-06-11 16:13:01 +0300

    remove

--- service/userData-service.go
+++ service/userData-service.go
@@ -1,22 +1,17 @@
 package service
 
-import (
-	"fmt"
-	"github.com/adshao/go-binance/v2"
-)
-
-func userData() {
-
-	wsHandler := func(message []byte) {
-		fmt.Println(string(message))
-	}
-	errHandler := func(err error) {
-		fmt.Println(err)
-	}
-	doneC, _, err := binance.WsUserDataServe(listenKey, wsHandler, errHandler)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	<-doneC
-}
+//func userData() {
+//
+//	wsHandler := func(message []byte) {
+//		fmt.Println(string(message))
+//	}
+//	errHandler := func(err error) {
+//		fmt.Println(err)
+//	}
+//	doneC, _, err := binance.WsUserDataServe(listenKey, wsHandler, errHandler)
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//	<-doneC
+//}
